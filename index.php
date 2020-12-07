d097810b431a0ac01ba2dd1dd8974653212ea9d9
cmhansen125 <48603387+cmhansen125@users.noreply.github.com>
2020-12-07 18:14:47 -0500

    Other Files from Website Folder

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: http://www.technowolves.org/Home");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+</body>
+</html>
---
+++ indexmaintenance.php
@@ -0,0 +1,63 @@
+<?php
+    header("HTTP/1.0 503 – Service Unavailable");
+?>
+
+<html lang="en" class="gr__getbootstrap_com">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>TW 5518</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/floating-labels/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="/docs/4.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="floating-labels.css" rel="stylesheet">
+</head>
+
+<body data-gr-c-s-loaded="true" style="    display: flex;align-items: center;justify-content: center;">
+    <form class="form-signin" pb-autologin="true" autocomplete="off" style="margin:0; width:75%;">
+        <div class="text-center mb-4">
+            <i class="fas fa-exclamation-triangle" style="width: 72px; height: 72px;"></i>
+            <script defer src="https://use.fontawesome.com/releases/v5.8.1/js/all.js" integrity="sha384-g5uSoOSBd7KkhAMlnQILrecXvzst9TdC09/VM+pjDTCM+1il8RHz5fKANTFFb+gQ" crossorigin="anonymous"></script>
+            <h1 class="h3 mb-3 font-weight-normal">Error 503 - Maintenance in Progress</h1>
+            <p>A sneaky fox broke in and chewed up all our wires! Our maintenance wolves are hard at work though, and we'll be back by tomorrow at 3 PM Eastern. Sorry for the inconvenience!</p>
+        </div>
+    </form>
+
+</body>
+
+</html>
