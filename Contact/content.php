cd13fbb33a6dca37ae9ce9449abdd064b05d61fa
cmhansen125 <48603387+cmhansen125@users.noreply.github.com>
2020-12-07 18:06:37 -0500

    Contact Folder

---
+++ Contact/content.php
@@ -0,0 +1,44 @@
+<div class="containerwrapper">
+    <div class="container">
+        <div class="row">
+            <div class="column">
+                <h1>Contact Us</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="containerwrapper">
+    <div class="container">
+        <div class="row">
+            <div class="column">
+                <h2>Communication</h2>
+            </div>
+        </div>
+        <div class="row">
+            <!-- <div class="column">
+                <div class="panel">
+                    <h3>Phone</h3>
+                    <p>(910) 556-9712</p>
+                </div>
+            </div> -->
+            <div class="column">
+                <div class="panel">
+                    <h3>Email</h3>
+                    <p>tw5518@technowolves.org</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<div class="containerwrapper">
+    <div class="container">
+        <div class="row">
+            <div class="column">
+                <h2>Location</h2>
+                <iframe id="gmap_canvas" src="https://maps.google.com/maps?q=2516%20Reliance%20Avenue&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net"></a>
+            </div>
+
+        </div>
+    </div>
+</div>
