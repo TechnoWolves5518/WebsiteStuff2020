65fab5cd81682f7039fe625fee78eb9f88b17464
cmhansen125 <48603387+cmhansen125@users.noreply.github.com>
2020-12-07 18:05:20 -0500

    About Folder

---
+++ About/content.php
@@ -0,0 +1,86 @@
+<div class="containerwrapper" id="pageheader">
+    <div class="container">
+        <div class="row">
+            <div class="column">
+                <h1>About</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="containerwrapper" id="quickstats">
+    <div class="container">
+
+        <div class="row">
+            <div class="column">
+                <div class="panel">
+                    <h1>05</h1>
+                    <p>Years Running</p>
+                </div>
+            </div>
+            <div class="column">
+                <div class="panel">
+                    <h1>30</h1>
+                    <p>Active Members</p>
+                </div>
+            </div>
+            <div class="column">
+                <div class="panel">
+                    <h1>08</h1>
+                    <p>Awards Won</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="containerwrapper" id="aboutus">
+    <div class="container">
+        <div class="row">
+            <div class="column" id="overviewaboutus">
+                <h2>The Team</h2>
+                <p>
+                    Techno Wolves FRC Team 5518 is a high school student-led FIRST Robotics Competition team based in Apex, North Carolina. We
+                    are a fourth-year community-based team partnered with Wake STEM Early College High School at NC State
+                    University. Techno Wolves serves as a valuable learning experience to all of our members; the team
+                    structure is based off of an existing model in the corporate world, giving members an early, competitive
+                    edge when applying for jobs and college. Additionally, our robotics program encourages students to
+                    actively promote the team by attending outreach events, participating in community service, raising
+                    funds, and reaching out to local businesses/organizations to support us.
+                </p>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="column" id="missionaboutus">
+                <div class="panel">
+                    <p>
+                        Our Mission - “To enrich the minds of high school students by promoting an innovative learning experience with our community
+                        – such as local businesses, schools, and mentors – via the exploration of science, technology,
+                        engineering, arts, and mathematics (STEAM) through the exposure of robotics and business management
+                        principles.”
+                    </p>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</div>
+<div class="containerwrapper" id="aboutfirst">
+    <div class="container">
+        <div class="row">
+            <div class="column" id="overviewaboutfirst">
+                <h2>The FIRST Program</h2>
+                <p>Combining the excitement of sport with the rigors of science and technology. We call FIRST Robotics Competition
+                    the ultimate Sport for the Mind. High-school student participants call it “the hardest fun you’ll
+                    ever have.”</p>
+                <p class="button">
+                    <a href="https://www.firstinspires.org/robotics/frc">FIRST's Website</a>
+                </p>
+            </div>
+            <div class="column" id="videoaboutfirst">
+                <iframe src="https://www.youtube.com/embed/wSA6InzFAZE">
+                </iframe>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ About/index.php
@@ -0,0 +1,19 @@
+<?php
+$upOne = realpath(dirname(__FILE__) . '/..');
+include $upOne . '/SiteWideData/initialscripts.php';
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <title>About: TW 5518</title>
+    <?php include $head;?>
+</head>
+
+<body>
+    <?php include $navbar;?>
+    <?php include 'content.php';?>
+    <?php include $footer;?>
+</body>
